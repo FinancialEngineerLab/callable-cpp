786d4fa6b817495372da00c310c68f2bf6554d55
si-wu-physics <wusi1983@gmail.com>
2018-06-25 21:45:54 -0700

    added dividend policy

---
+++ dividend_policy.cpp
@@ -0,0 +1,54 @@
+#include "dividend_policy.hpp"
+
+namespace beagle
+{
+
+  namespace impl
+  {
+    struct LiquidatorDividendPolicy : public DividendPolicy
+    {
+      LiquidatorDividendPolicy( void )
+      { }
+      virtual ~LiquidatorDividendPolicy( void )
+      { }
+    public:
+      virtual double exDividendStockPrice( double spot,
+                                           double dividend ) const override
+      {
+        return spot > dividend ? spot - dividend : 0.;
+      }
+    };
+
+    struct SurvivorDividendPolicy : public DividendPolicy
+    {
+      SurvivorDividendPolicy( void )
+      { }
+      virtual ~SurvivorDividendPolicy( void )
+      { }
+    public:
+      virtual double exDividendStockPrice( double spot,
+                                           double dividend ) const override
+      {
+        return spot > dividend ? spot - dividend : spot;
+      }
+    };
+  }
+
+  DividendPolicy::DividendPolicy( void )
+  { }
+
+  DividendPolicy::~DividendPolicy( void )
+  { }
+
+  dividend_policy_ptr_t
+  DividendPolicy::liquidator( void )
+  {
+    return std::make_shared<impl::LiquidatorDividendPolicy>();
+  }
+
+  dividend_policy_ptr_t
+  DividendPolicy::survivor( void )
+  {
+    return std::make_shared<impl::SurvivorDividendPolicy>();
+  }
+}
