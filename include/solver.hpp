2c00db50f74b0eac6268f973cc1fe83d9d1388c2
si-wu-physics <wusi1983@gmail.com>
2020-03-07 20:59:59 -0800

    new solver

--- include/solver.hpp
+++ include/solver.hpp
@@ -12,7 +12,8 @@ namespace beagle
       BoundaryCondition( void );
       virtual ~BoundaryCondition( void );
     public:
-      virtual beagle::dbl_vec_t boundaryCoefficients( void ) const = 0;
+      virtual beagle::dbl_vec_t lowerBoundaryCoefficients( void ) const = 0;
+      virtual beagle::dbl_vec_t upperBoundaryCoefficients( void ) const = 0;
     public:
       static beagle::boundary_condition_ptr_t naturalBoundaryCondition( void );
       static beagle::boundary_condition_ptr_t linearBoundaryCondition( void );
@@ -21,21 +22,20 @@ namespace beagle
     struct OneDimParabolicPDESolver
     {
       OneDimParabolicPDESolver(double spot,
-                               const beagle::real_2d_function_ptr_t& drift,
-                               const beagle::real_2d_function_ptr_t& volatility,
+                               const beagle::real_2d_function_ptr_t& convection,
+                               const beagle::real_2d_function_ptr_t& diffusion,
                                const beagle::real_function_ptr_t& rate,
                                const beagle::discrete_dividend_schedule_t& dividends,
                                const beagle::valuation::OneDimFiniteDifferenceSettings& settings);
     public:
-      void evolve(double start,
-                  double end,
-                  const beagle::boundary_condition_ptr_t& lowerBoundaryCondition,
-                  const beagle::boundary_condition_ptr_t& upperBoundaryCondition,
-                  beagle::real_function_ptr_t& initialValues) const;
+      beagle::real_function_ptr_t evolve(double start,
+                                         double end,
+                                         const beagle::boundary_condition_ptr_t& boundaryCondition,
+                                         const beagle::real_function_ptr_t& initialCondition) const;
     private:
       double m_Spot;
-      beagle::real_2d_function_ptr_t m_Drift;
-      beagle::real_2d_function_ptr_t m_Vol;
+      beagle::real_2d_function_ptr_t m_Convection;
+      beagle::real_2d_function_ptr_t m_Diffusion;
       beagle::real_function_ptr_t m_Rate;
       beagle::discrete_dividend_schedule_t m_Dividends;
       beagle::valuation::OneDimFiniteDifferenceSettings m_Settings;
--- src/solver.cpp
+++ src/solver.cpp
@@ -1,4 +1,7 @@
 #include "solver.hpp"
+#include "real_function.hpp"
+#include "real_2d_function.hpp"
+#include "util.hpp"
 
 namespace beagle
 {
@@ -11,9 +14,13 @@ namespace beagle
         virtual ~NaturalBoundaryCondition( void )
         { }
       public:
-        virtual beagle::dbl_vec_t boundaryCoefficients( void ) const
+        virtual beagle::dbl_vec_t lowerBoundaryCoefficients( void ) const
         {
-          return beagle::dbl_vec_t{};
+          return beagle::dbl_vec_t{0., 1., 0., 0.};
+        }
+        virtual beagle::dbl_vec_t upperBoundaryCoefficients( void ) const
+        {
+          return beagle::dbl_vec_t{0., 0., 0., 1.};
         }
       };
       
@@ -22,9 +29,13 @@ namespace beagle
         virtual ~LinearBoundaryCondition( void )
         { }
       public:
-        virtual beagle::dbl_vec_t boundaryCoefficients( void ) const
+        virtual beagle::dbl_vec_t lowerBoundaryCoefficients( void ) const
         {
-          return beagle::dbl_vec_t{};
+          return beagle::dbl_vec_t{0., 1., -2., 1.};
+        }
+        virtual beagle::dbl_vec_t upperBoundaryCoefficients( void ) const
+        {
+          return beagle::dbl_vec_t{0., 1., -2., 1.};
         }
       };
     }
@@ -48,26 +59,100 @@ namespace beagle
     }
 
     OneDimParabolicPDESolver::OneDimParabolicPDESolver(double spot,
-                                                       const beagle::real_2d_function_ptr_t& drift,
-                                                       const beagle::real_2d_function_ptr_t& volatility,
+                                                       const beagle::real_2d_function_ptr_t& convection,
+                                                       const beagle::real_2d_function_ptr_t& diffusion,
                                                        const beagle::real_function_ptr_t& rate,
                                                        const beagle::discrete_dividend_schedule_t& dividends,
                                                        const beagle::valuation::OneDimFiniteDifferenceSettings& settings) :
       m_Spot(spot),
-      m_Drift(drift),
-      m_Vol(volatility),
+      m_Convection(convection),
+      m_Diffusion(diffusion),
       m_Rate(rate),
       m_Dividends(dividends),
       m_Settings(settings)
     { }
 
-    void OneDimParabolicPDESolver::evolve(double start,
-                                          double end,
-                                          const beagle::boundary_condition_ptr_t& lowerBoundaryCondition,
-                                          const beagle::boundary_condition_ptr_t& upperBoundaryCondition,
-                                          beagle::real_function_ptr_t& initialValues) const
+    beagle::real_function_ptr_t
+    OneDimParabolicPDESolver::evolve(double start,
+                                     double end,
+                                     const beagle::boundary_condition_ptr_t& boundaryCondition,
+                                     const beagle::real_function_ptr_t& initialCondition) const
     {
-      
+      // Form time steps
+      beagle::dbl_vec_t times;
+      double expiry = end - start;
+      int numTimeSteps = static_cast<int>(std::floor(expiry * m_Settings.numberOfTimeSteps()));
+      times.resize(numTimeSteps + 1);
+      for (int i=0; i<numTimeSteps+1; ++i)
+        times[i] = start + i * expiry / numTimeSteps;
+
+      // Form underlying steps
+      // Notice we are writing the PDE in the log price space, the drift and the 
+      // volatility should be evaluated at the exponential of the lattice points
+
+      // Two extra points are reserved for boundary conditions
+      int numUnderlyingSteps = m_Settings.numberOfStateVariableSteps();
+      beagle::dbl_vec_t underlyings(numUnderlyingSteps);
+
+      double center = std::log(m_Spot) + expiry * m_Convection->value(end, m_Spot);
+      double spread = m_Settings.numberOfGaussianStandardDeviations()
+                    * std::sqrt(2. * m_Diffusion->value(end, m_Spot) * end);
+
+      int centralIndex = numUnderlyingSteps / 2;
+      double deltaX = spread / centralIndex;
+      for (int i=0; i<numUnderlyingSteps; ++i)
+        underlyings[i] = center + (i - centralIndex) * deltaX;
+
+      // Discretize the intial condition
+      beagle::dbl_vec_t prices(numUnderlyingSteps);
+      std::transform(underlyings.cbegin(),
+                     underlyings.cend(),
+                     prices.begin(),
+                     [&initialCondition](double z)
+                     { return initialCondition->value(std::exp(z)); });
+
+      // Now, perform induction
+      beagle::dbl_vec_t diag(numUnderlyingSteps);
+      beagle::dbl_vec_t lower(numUnderlyingSteps);
+      beagle::dbl_vec_t upper(numUnderlyingSteps);
+
+      for (int i=0; i<numTimeSteps-1; ++i)
+      {
+        double thisTime = times[i+1];
+        double deltaT = thisTime - times[i];
+        double dTdX = deltaT / deltaX;
+        double dTdXdX = dTdX / deltaX;
+
+        for (int j=0; j<numUnderlyingSteps; ++j)
+        {
+          double thisSpot = std::exp(underlyings[j]);
+          double convection = m_Convection->value(thisTime, thisSpot);
+          double diffusion = m_Diffusion->value(thisTime, thisSpot);
+          double rate = m_Rate->value(thisTime);
+
+          diag[j]  = 1. - rate * deltaT - 2. * diffusion * dTdXdX;
+          upper[j] =   .5 * convection * dTdX + diffusion * dTdXdX;
+          lower[j] = - .5 * convection * dTdX + diffusion * dTdXdX;
+        }
+
+        beagle::dbl_vec_t lbc = boundaryCondition->lowerBoundaryCoefficients();
+        prices[0] -= lower[0] * lbc[0] / lbc[1];
+        diag[0]   -= lower[0] * lbc[2] / lbc[1];
+        upper[0]  -= lower[0] * lbc[3] / lbc[1];
+
+        beagle::dbl_vec_t ubc = boundaryCondition->upperBoundaryCoefficients();
+        prices[numUnderlyingSteps-1] -= upper[numUnderlyingSteps-1] * ubc[0] / ubc[3];
+        diag[numUnderlyingSteps-1]   -= upper[numUnderlyingSteps-1] * ubc[1] / ubc[3];
+        upper[numUnderlyingSteps-1]  -= upper[numUnderlyingSteps-1] * ubc[2] / ubc[3];
+
+        // for (auto price : prices)
+        //   out << price << " ";
+        // out << std::endl;
+
+        beagle::util::tridiagonalSolve( prices, diag, upper, lower );
+      }
+
+      return beagle::real_function_ptr_t();
     }
   }
 }
