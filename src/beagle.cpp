cddd79978c2fe185e0176b3abd29a10e855c92c1
si-wu-physics <wusi1983@gmail.com>
2021-02-26 15:03:59 -0800

    renamed the main cpp file in src folder

---
+++ src/beagle.cpp
@@ -0,0 +1,27 @@
+#include "gocsei_sahel.hpp"
+#include "andersen_buffum.hpp"
+
+int main( void )
+{
+  // beagle::test::test_gocsei_sahel();
+  // beagle::test::test_local_vol_calibration();
+  // beagle::test::test_cev_finite_difference();
+  // beagle::test::test_tridiagonal_solver();
+  // beagle::test::test_implied_vol_credit_spread();
+  // beagle::test::test_bond_pricer();
+  // beagle::test::test_discontinuous_forward_curve();
+  // beagle::test::test_natural_cubic_spline();
+
+  // beagle::test::generateAndersenBuffumFigureTwo();
+  // beagle::test::generateAndersenBuffumFigureThree();
+  // beagle::test::generateAndersenBuffumFigureFour();
+  // beagle::test::generateAndersenBuffumFigureFive();
+  // beagle::test::generateAndersenBuffumFigureSix();
+  // beagle::test::generateAndersenBuffumFigureSeven();
+  beagle::test::generateAndersenBuffumTableOneCalibratedPrice();
+  beagle::test::generateAndersenBuffumTableOneNaivePrice();
+
+  // beagle::test::test_volatility_smile_credit_spread_calibration();
+
+  return 0;
+}
